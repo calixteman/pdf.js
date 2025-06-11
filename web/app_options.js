82da8f8d196ebd308961535e1249276ed415cb15
Calixte Denizet <calixte.denizet@gmail.com>
2025-06-11 15:36:04 +0200

    Add highlights.pdf

--- web/app_options.js
+++ web/app_options.js
@@ -533,7 +533,7 @@ if (typeof PDFJSDev === "undefined" || !PDFJSDev.test("MOZCENTRAL")) {
     value:
       typeof PDFJSDev !== "undefined" && PDFJSDev.test("CHROME")
         ? ""
-        : "compressed.tracemonkey-pldi-09.pdf",
+        : "highlights.pdf",
     kind: OptionKind.VIEWER,
   };
   defaultOptions.sandboxBundleSrc = {
