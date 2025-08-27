a4feabd02cf54b1a0f6c55850eaf0d0aa85d1898
Calixte Denizet <calixte.denizet@gmail.com>
2025-08-27 17:36:56 +0200

    Enable comment

--- web/app_options.js
+++ web/app_options.js
@@ -220,7 +220,7 @@ const defaultOptions = {
   },
   enableComment: {
     /** @type {boolean} */
-    value: typeof PDFJSDev === "undefined" || PDFJSDev.test("TESTING"),
+    value: true, //typeof PDFJSDev === "undefined" || PDFJSDev.test("TESTING"),
     kind: OptionKind.VIEWER + OptionKind.PREFERENCE,
   },
   enableDetailCanvas: {
