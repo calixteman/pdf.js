fc9a2517a99ed71974ff22891477760dd8860ace
Calixte Denizet <calixte.denizet@gmail.com>
2026-01-08 20:11:54 +0100

    Add page_with_number.pdf as default

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1141,7 +1141,7 @@ function buildGeneric(defines, dir) {
       .pipe(gulp.dest(dir + "web")),
 
     gulp
-      .src("web/compressed.tracemonkey-pldi-09.pdf", { encoding: false })
+      .src("web/page_with_number.pdf", { encoding: false })
       .pipe(gulp.dest(dir + "web")),
   ]);
 }
